3f8dc8ec16addc272fe6a2d8f7f34354d135c2fe
Andrii Semeniuk <semtrololo@gmail.com>
2025-04-09 23:47:21 +0200

    Initial commit

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,36 @@
+// Ця функція повинна приймати один параметр query (пошукове слово, яке є рядком),
+// здійснювати HTTP-запит і повертати значення властивості data з отриманої відповіді.
+import axios from 'axios';
+
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '49684635-7a6aa23e2b6de301ea62c53b0';
+
+export default function getImagesByQuery(searchText) {
+  return axios
+    .get(`${BASE_URL}`, {
+      params: {
+        key: API_KEY,
+        q: searchText,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: 'true',
+        per_page: 21,
+      },
+    })
+
+    .then(response => {
+      if (!response.data.hits.length) {
+        iziToast.error({
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+          position: 'topRight',
+        });
+      }
+      return response.data.hits;
+    })
+
+    .catch();
+}
